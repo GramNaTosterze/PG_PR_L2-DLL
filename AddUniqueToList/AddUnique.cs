6320bd011d64a2f3a3857d32af1df72aa9169356
Krzysztof Napiórkowski <krzysna@gmail.com>
2023-04-12 21:25:06 +0200

    Full

---
+++ AddUniqueToList/AddUnique.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PR_Lab2
+{
+    static public class AddUnique
+    {
+        static public void Try_add(List<int> list, int element, string thread)
+        {
+            lock (list)
+            {
+                int idx = list.IndexOf(element);
+                if (idx >= 0)
+                {
+                    Console.WriteLine("F " + element + " " + thread);
+                    return;
+
+                }
+                list.Add(element);
+                Console.WriteLine("T " + element + " " + thread);
+                return;
+
+            }
+        }
+        static public void Add_some(List<int> list, string t)
+        {
+            for (int i = 0; i < 1000; i++)
+                Try_add(list, i, t);
+        }
+    }
+}
---
+++ PR_Lab2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+/* Wielowątkowe dodawanie do struktury zawartej w dll */
+namespace PR_Lab2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+            Thread t1 = new Thread(() => AddUnique.Add_some(list, "t1"));
+            Thread t2 = new Thread(() => AddUnique.Add_some(list, "t2"));
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+        }
+    }
+}
